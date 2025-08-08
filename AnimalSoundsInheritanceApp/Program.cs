c8f91044a0c481519ddff1fd7aac9840d3041131
NaaShidaaAddo <nsaaddo@st.ug.edu.gh>
2025-08-08 11:29:11 +0000

    Added Inheritance and Method Overriding App - Animal sounds

---
+++ AnimalSoundsInheritanceApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class Animal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Some generic sound");
+    }
+}
+
+public class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Bark");
+    }
+}
+
+public class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meow");
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Animal genericAnimal = new Animal();
+        Animal dog = new Dog();
+        Animal cat = new Cat();
+
+        Console.WriteLine("Generic Animal Sound:");
+        genericAnimal.MakeSound();
+        Console.WriteLine("Dog Sound:");
+        dog.MakeSound();
+        Console.WriteLine("Cat Sound:");
+        cat.MakeSound();
+    }
+}
